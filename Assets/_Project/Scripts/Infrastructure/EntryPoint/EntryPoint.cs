60a38303b58e317d2ea21c60f6baaae02730b528
RomanSharipov <roman.sharipov.1990@gmail.com>
2026-01-29 23:34:49 +0500

    refactoring

--- Assets/_Project/Scripts/Infrastructure/EntryPoint/EntryPoint.cs
+++ Assets/_Project/Scripts/Infrastructure/EntryPoint/EntryPoint.cs
@@ -9,21 +9,15 @@ namespace CodeBase.Infrastructure
     public class EntryPoint : IAsyncStartable
     {
         [Inject] private readonly IAppStateService _appStateService;
-
-        
         [Inject] private readonly BootstrapState _bootstrapState;
         [Inject] private readonly MenuState _menuState;
         [Inject] private readonly GameLoopState _gameLoopState;
-
-
+        
         public async UniTask StartAsync(CancellationToken cancellation = default)
         {
-            
             _appStateService.AddState(_bootstrapState);
             _appStateService.AddState(_menuState);
             _appStateService.AddState(_gameLoopState);
-
-
             
             await _appStateService.Enter<BootstrapState>();
         }
